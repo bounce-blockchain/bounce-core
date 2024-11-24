a20a9ea9ccd92c6f3cf965f74190627720d21ad2
lapisliu <lxteng2016@126.com>
2024-11-24 16:48:20 -0500

    test: data processing

--- distributed-timming-tests/src/dataloader.rs
+++ distributed-timming-tests/src/dataloader.rs
@@ -1,12 +1,35 @@
 use std::fs;
+use std::fs::File;
+use std::io::{Read, Write};
+use serde_json::to_writer_pretty;
+
 
 fn main() -> std::io::Result<()> {
-    let content = fs::read("receiving_time_elapsed.bin")?;
-    let data: Vec<Vec<u128>> = bincode::deserialize(&content).expect("Failed to deserialize");
-    println!("Loaded data: {:?}", data);
+    // let content = fs::read("receiving_time_elapsed_5.bin")?;
+    // let data: Vec<Vec<u128>> = bincode::deserialize(&content).expect("Failed to deserialize");
+    // println!("Loaded data: {:?}", data);
+    let mut data: Vec<Vec<u128>> = Vec::new();
+    for i in 0..10 {
+        let content = fs::read(format!("receiving_time_elapsed_{}.bin", i))?;
+        let cur_data: Vec<Vec<u128>> = bincode::deserialize(&content).expect("Failed to deserialize");
+        data.extend(cur_data);
+    }
+
+
     for d in &data {
         println!("{:?}", d);
     }
     println!("Data length: {}", data.len());
+    // let mut sum = 0;
+    // for d in &data[19] {
+    //     sum += d;
+    // }
+    // println!("Average: {}", sum / data[19].len() as u128);
+
+    // Write the data as JSON to a file
+    let json_file = File::create("receiving_time_elapsed_100_benchmarks.json").unwrap();
+    to_writer_pretty(json_file, &data).unwrap();
+    //
+    println!("Data has been reserialized into JSON");
     Ok(())
 }
